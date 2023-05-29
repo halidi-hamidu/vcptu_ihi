07fbcf446d3c17bd003c92b4dbb6c1a454288c64
alfani <alfanishekw@gmail.com>
2023-05-29 11:28:11 +0300

    Third commit

--- vcptuWebsite/settings.py
+++ vcptuWebsite/settings.py
@@ -31,7 +31,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-CSRF_TRUSTED_ORIGINS =['https://vcptuihi.up.railway.app/','https://*.127.0.0.1']
+CSRF_TRUSTED_ORIGINS =['https://vcptuihi.up.railway.app','https://*.127.0.0.1']
 
 # Application definition
 
