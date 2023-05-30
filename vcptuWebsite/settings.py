e551930e21e686f9724257ad4a5639604b08b632
alfani <alfanishekw@gmail.com>
2023-05-30 14:01:19 +0300

    New publication list added

--- vcptuWebsite/settings.py
+++ vcptuWebsite/settings.py
@@ -134,12 +134,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT =os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [
-    BASE_DIR /"static"
-    ]
-# STATICFILES_STORAGE="whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_DIRS = (
+   os.path.join(BASE_DIR, 'static'),
+)
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'images')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- vcptuWebsite/urls.py
+++ vcptuWebsite/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('vcptu_home.urls', namespace='vcptu_home')),
+    path('', include('vcptu_home.urls')),
 ]
 
 if settings.DEBUG:
