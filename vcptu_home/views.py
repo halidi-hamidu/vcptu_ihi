b50a47c882a0d5ce34771868a372fca714f29a3e
alfani <alfanishekw@gmail.com>
2023-05-30 15:46:23 +0300

    New publication added

--- vcptu_home/views.py
+++ vcptu_home/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
-
+from vcptu_publication.models import *
 # Create your views here.
-\
+
 def homePage(request):
+    get_latest_publication = PublicationModel.objects.all().order_by('-created_at')[0:2]
     template_name = 'homePage/homePage.html'
     context ={
-
+        'get_latest_publication':get_latest_publication
     }
     return render(request, template_name, context)
