9803257dc1b08760295e44068dc8a65a6fba6102
alfani <alfanishekw@gmail.com>
2023-05-30 13:42:31 +0300

    Publication Model added

--- vcptu_publication/models.py
+++ vcptu_publication/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
+import uuid
 # Create your models here.
+class PublicationModel(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True, primary_key=True)
+    publication_title = models.CharField(max_length=5000, null=True, blank=True)
+    publication_contributors = models.CharField(max_length=5000, null=True, blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    # always change when object is updated
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name_plural = 'List of Publications'
+
+    def __str__(self):
+        return str(self.publication_title)
