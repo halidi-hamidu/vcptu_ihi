88e5853a6bd98e03682fe4aa316a898f9ed93a56
alfani <alfanishekw@gmail.com>
2023-05-30 16:56:51 +0300

    Staff added

--- vcptu_home/views.py
+++ vcptu_home/views.py
@@ -1,11 +1,22 @@
 from django.shortcuts import render
 from vcptu_publication.models import *
+from vcptu_team.models import *
 # Create your views here.
 
 def homePage(request):
     get_latest_publication = PublicationModel.objects.all().order_by('-created_at')[0:2]
+
+    get_head_of_unit = TeamModel.objects.get(staff_title ='Head of Unit')
+    get_test_facility_manager = TeamModel.objects.get(staff_title ='Test Facility Manager')
+    get_study_director = TeamModel.objects.get(staff_title ='Study Director')
+    get_project_manager = TeamModel.objects.get(staff_title ='Project Manager')
     template_name = 'homePage/homePage.html'
     context ={
-        'get_latest_publication':get_latest_publication
+        'get_latest_publication':get_latest_publication,
+        'get_head_of_unit':get_head_of_unit,
+        'get_test_facility_manager':get_test_facility_manager,
+        'get_study_director':get_study_director,
+        'get_project_manager':get_project_manager
+
     }
     return render(request, template_name, context)
--- vcptu_team/models.py
+++ vcptu_team/models.py
@@ -1,3 +1,30 @@
 from django.db import models
-
+import uuid
 # Create your models here.
+class TeamModel(models.Model):
+    STAFF_TITLE = (
+        ('', 'Select Title'),
+        ('Head of Unit', 'Head of Unit'),
+        ('Test Facility Manager', 'Test Facility Manager'),
+        ('Study Director', 'Study Director'),
+        ('Project Manager', 'Project Manager'),
+        ('Study Coordinator', 'Study Coordinator'),
+        ('Technician', 'Technician'),
+        ('Test Facility Coordinator', 'Test Facility Coordinator'),
+        ('Administrative Officer', 'Administrative Officer'),
+
+    )
+    id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True, primary_key=True)
+    image = models.ImageField(upload_to='media')
+    staff_name = models.CharField(max_length=100, null=True, blank=True)
+    staff_title = models.CharField(max_length=300, null=True, blank=True, choices=STAFF_TITLE)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    # always change when object is updated
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name_plural = 'VCPTU Team'
+
+    def __str__(self):
+        return str(self.staff_name)
