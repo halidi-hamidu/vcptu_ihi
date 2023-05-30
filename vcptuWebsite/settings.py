f1ff33c31953693509b745fa5ff48358376f15f8
alfani <alfanishekw@gmail.com>
2023-05-30 09:43:50 +0300

    Background video added

--- vcptuWebsite/settings.py
+++ vcptuWebsite/settings.py
@@ -134,11 +134,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT =os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT =os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'staticfiles')
+    BASE_DIR /"static"
     ]
-STATICFILES_STORAGE="whitenoise.storage.CompressedManifestStaticFilesStorage"
+# STATICFILES_STORAGE="whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
 # Default primary key field type
