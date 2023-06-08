509c9296396a2ebb642c8d4cf696e0f083197ce5
alfani <alfanishekw@gmail.com>
2023-06-08 15:05:48 +0300

    News page added

--- vcptu_news/views.py
+++ vcptu_news/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
-
+from .models import NewsModel
 # Create your views here.
 def newsPage(request):
+
+    get_all_news = NewsModel.objects.all().order_by('-created_at')
     template_name = 'newsPage/newsPage.html'
     context = {
-
+        'get_all_news':get_all_news
     }
     return render(request, template_name, context)
