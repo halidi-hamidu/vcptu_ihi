608172a613a36d8612c898a3b46a8d8d30b704e8
alfani <alfanishekw@gmail.com>
2023-06-09 11:47:41 +0300

    Website Small scren responsive fixed

--- vcptu_home/views.py
+++ vcptu_home/views.py
@@ -6,22 +6,32 @@ from vcptu_news.models import NewsModel
 
 
 def homePage(request):
-    get_latest_news = NewsModel.objects.all().order_by('-created_at')[0:3]
+    try:
+        get_latest_news = NewsModel.objects.all().order_by('-created_at')[0:3]
 
-    get_latest_publication = PublicationModel.objects.all().order_by('-created_at')[0:2]
+        get_latest_publication = PublicationModel.objects.all().order_by('-created_at')[0:2]
 
-    get_head_of_unit = TeamModel.objects.get(staff_title ='Head of Unit')
-    get_test_facility_manager = TeamModel.objects.get(staff_title ='Test Facility Manager')
-    get_study_director = TeamModel.objects.get(staff_title ='Study Director')
-    get_project_manager = TeamModel.objects.get(staff_title ='Project Manager')
-    template_name = 'homePage/homePage.html'
-    context ={
-        'get_latest_publication':get_latest_publication,
-        'get_head_of_unit':get_head_of_unit,
-        'get_test_facility_manager':get_test_facility_manager,
-        'get_study_director':get_study_director,
-        'get_project_manager':get_project_manager,
-        'get_latest_news':get_latest_news,
+        get_head_of_unit = TeamModel.objects.get(staff_title ='Head of Unit')
+        get_test_facility_manager = TeamModel.objects.get(staff_title ='Test Facility Manager')
+        get_study_director = TeamModel.objects.get(staff_title ='Study Director')
+        get_project_manager = TeamModel.objects.get(staff_title ='Project Manager')
+        get_all_team_leader = TeamModel.objects.all()
+        template_name = 'homePage/homePage.html'
+        context ={
+            'get_latest_publication':get_latest_publication,
+            'get_head_of_unit':get_head_of_unit,
+            'get_test_facility_manager':get_test_facility_manager,
+            'get_study_director':get_study_director,
+            'get_project_manager':get_project_manager,
+            'get_latest_news':get_latest_news,
+            'get_all_team_leader':get_all_team_leader
 
-    }
-    return render(request, template_name, context)
+        }
+        return render(request, template_name, context)
+    except:
+        template_name = 'ErrorPage/page404.html'
+        context = {
+
+        }
+        return render  (request, template_name, context)
+    
